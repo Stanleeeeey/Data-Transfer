30f0b1173369d231dc4fc07ff8d5947ce6285b96
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2022-04-01 19:00:04 +0200

    Update SERVERPROTO.hpp
    Final Server.hpp,

--- Server/SERVERPROTO.hpp
+++ Server/SERVERPROTO.hpp
@@ -24,8 +24,12 @@
 #include <iomanip>
 #include <thread>
 #include <tchar.h>
+#include <chrono>
+#include <atomic>
+
 
 using namespace std;
+using namespace std::chrono;
 
 // Zmienne globalne
 #define PORT 6666
@@ -33,20 +37,13 @@ using namespace std;
 #define BUFF 10
 #define BUFFID 3
 #define ENDING "<!--END"
-string TYPES[5] =  {"MOU", "SCR", "MIC", "KEY", "SOU"};
 
-
-struct Message {
-    int8_t type;
-    int8_t id;
-    char content[BUFF-BUFFID];
-
-};
 
 struct Send_args {
     string Batched[1000];
-    
+
 };
+
 
 SOCKET SendSock = INVALID_SOCKET;
 SOCKET RecvSock = INVALID_SOCKET;
@@ -55,15 +52,11 @@ struct sockaddr_in SendSockAddr;
 struct sockaddr_in RecvSockAddr;
 
 char SendMsgBuffered[BUFF + BUFFID + 1];
-char RecvBufferedMsg[BUFFID + 1]; 
+char RecvBufferedMsg[BUFFID + 1];
 
-string Batched[1000];
-
-//COMPILE COMMAND 
-//g++ -o prog.exe RPserver.cpp -lws2_32 -pthread
 
 //INITIALIZE WINSOCK LIBRARY
-int InitializeWinSock(){
+int InitializeWinSock() {
 
 
     WSADATA wsaData;
@@ -79,16 +72,16 @@ int InitializeWinSock(){
 }
 
 //INITIALIZE SOCKET 
-int InitializeSocket(){
+int InitializeSocket() {
 
 
     SendSock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
     RecvSock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
-    if (SendSock != INVALID_SOCKET && RecvSock != INVALID_SOCKET){
+    if (SendSock != INVALID_SOCKET && RecvSock != INVALID_SOCKET) {
         printf("//  Sockets succesfully initialized\n");
         return 0;
-        
+
     }
 
     printf("//  Sockets initialization failed %d\n", WSAGetLastError());
@@ -96,20 +89,27 @@ int InitializeSocket(){
 
 }
 
+int CloseSocket() {
+
+    closesocket(SendSock);
+    closesocket(RecvSock);
+    WSACleanup();
+    return 1;
+}
 //BUILD SOCKET
-int BuildSocket(){
+int BuildSocket() {
 
 
     SendSockAddr.sin_family = AF_INET;
-    SendSockAddr.sin_port   = htons(PORT);
+    SendSockAddr.sin_port = htons(PORT);
 
     RecvSockAddr.sin_family = AF_INET;
-    RecvSockAddr.sin_port   = htons(PORT+1);
+    RecvSockAddr.sin_port = htons(PORT + 1);
 
     InetPton(AF_INET, _T(HOST), &RecvSockAddr.sin_addr.s_addr);
     InetPton(AF_INET, _T(HOST), &SendSockAddr.sin_addr.s_addr);
 
-    if ( bind(RecvSock, (SOCKADDR *) & RecvSockAddr, sizeof (RecvSockAddr))) {
+    if (bind(RecvSock, (SOCKADDR*)&RecvSockAddr, sizeof(RecvSockAddr))) {
         printf("//  bind failed with error %d\n", WSAGetLastError());
         return 1;
     }
@@ -119,14 +119,16 @@ int BuildSocket(){
 
 }
 
-//RECIVING 
+
+
+//RECIVING returns data packet id
 int ReciveFunc() {
-    
-    
-    int ServerAddrSize  = sizeof(RecvSockAddr);
 
 
-    int recived = recvfrom(RecvSock, RecvBufferedMsg, BUFFID, 0 , (SOCKADDR *)& RecvSockAddr, &ServerAddrSize);
+    int ServerAddrSize = sizeof(RecvSockAddr);
+
+
+    int recived = recvfrom(RecvSock, RecvBufferedMsg, BUFFID, 0, (SOCKADDR*)&RecvSockAddr, &ServerAddrSize);
 
     if (recived == SOCKET_ERROR) {
         printf("// recvfrom failed with error %d\n", WSAGetLastError());
@@ -135,200 +137,218 @@ int ReciveFunc() {
 
 
     RecvBufferedMsg[recived] = '\0';
-    int x; 
+    int x;
 
-    if (RecvBufferedMsg == "000"){
-        x= 0;
-    }else{
-    std::stringstream str(RecvBufferedMsg); 
-    str >> x;
+    if (RecvBufferedMsg == "000") {
+        x = 0;
+    }
+    else {
+        std::stringstream str(RecvBufferedMsg);
+        str >> x;
     }
 
-    //printf("//  succesfully recived :\n////  %s\n", RecvBufferedMsg);
     return x;
 
 }
 
-//SEND
-int SendFunc(std::string x){
+//SEND given string 
+int SendFunc(std::string x) {
 
 
 
-    for (int i =0; i<x.length(); i++) {
+    for (int i = 0; i < x.length(); i++) {
         SendMsgBuffered[i] = x[i];
-        }
+    }
 
     int MsgLen = (int)(x.length());
-    int result = sendto(SendSock, SendMsgBuffered, MsgLen, 0 , (SOCKADDR *) & SendSockAddr, sizeof(SendSockAddr));
+    int result = sendto(SendSock, SendMsgBuffered, MsgLen, 0, (SOCKADDR*)&SendSockAddr, sizeof(SendSockAddr));
 
     if (result == SOCKET_ERROR) {
-        //printf("// Sending back response got an error: %d\n", WSAGetLastError());
+        printf("// Sending back response got an error: %d\n", WSAGetLastError());
         return 1;
     }
 
-    //cout << SendMsgBuffered << endl;
+
     return 0;
 }
 
-//CONVERT TO BATCHES 
-int IntoBatches(string message, int type){
 
+//CONVERT TO BATCHES 
+Send_args IntoBatches(string message) {
 
     string Ending = ENDING;
-    message       = message + Ending;
+    message = message + Ending;
     string part;
 
+    int IdCount = BUFFID;
+    int LocalBUFF = BUFF;
+    int length = ceil((message.size()) * 1.0 / (LocalBUFF - 4));
 
-    int TypesCount = TYPES[0].length();
-    int IdCount    = BUFFID;
-    int LocalBUFF  = BUFF;
-    int length     = ceil((message.size())*1.0/(LocalBUFF-4));
 
-    
     if (LocalBUFF < Ending.size()) {
         LocalBUFF = Ending.size();
     }
 
-    string ans[1000];
+    Send_args ans;
 
-    while (length > 999){
-        cout<<"Increasing Buff Size ACTUAL BUFF SIZE:"<<LocalBUFF+1<<endl;
+    while (length > 999) {
+        cout << "Increasing Buff Size ACTUAL BUFF SIZE:" << LocalBUFF + 1 << endl;
 
-        LocalBUFF = LocalBUFF+1;
-        length    = ceil((message.size())*1.0 /(LocalBUFF-4));
+        LocalBUFF = LocalBUFF + 1;
+        length = ceil((message.size()) * 1.0 / (LocalBUFF - 4));
     }
 
-    for (int i = 0; i < length; i++){
-        
+    for (int i = 0; i < length; i++) {
+
         stringstream ss;
 
-        ss << setw(3) << setfill('0') << (i)%1000;
+        ss << setw(3) << setfill('0') << (i) % 1000;
         string s = ss.str();
-        
 
-        
-        if (message.size()>=(LocalBUFF-4)){
-            
-            part    = TYPES[type][0] + s + message.substr(0,LocalBUFF-4);
-            message = message.substr(LocalBUFF-4, message.size());
 
-            ans[i]  = part;
 
-        }else{
-            
-            part = "";
+        if (message.size() >= (LocalBUFF - 4)) {
 
-            part  = TYPES[type][0] + s + message;
+            ans.Batched[i] = s + message.substr(0, LocalBUFF - 4);
+            message = message.substr(LocalBUFF - 4, message.size());
 
-            
-            ans[i] = "";
-            ans[i] = part;
-
-            //cout << ans[i] << endl;
 
         }
-        
+        else {
+
+            ans.Batched[i] = s + message;
+
+        }
+
 
     }
 
-    for (int i = 0; i<length; i++){
-        Batched[i] = ans[i];
-    }
-    
 
-    
-    return 0;
+    return ans;
 }
 
-//BATCH AND SENDFUNC
-void Send(Send_args args){
+//SEND batched data
+void SendWithoutChecking(Send_args args) {
 
     printf("////    Send Thread succesfully started\n");
-    
-    
-    for (int i = 0; i<1000; i++){
+
+
+    for (int i = 0; i < 1000; i++) {
         int lost = rand() % 100 + 1;
-        
-        if (args.Batched[i] == ""){
+
+        if (args.Batched[i] == "") {
             break;
         }
 
-        if (lost > 5) {
-            SendFunc(args.Batched[i]);
-        }
-        
+
+        SendFunc(args.Batched[i]);
+
     }
 
-    cout<<"////     Ended SendingThread"<<endl;
-    
-    
-    
+    cout << "////     Ended Sending Thread" << endl;
+
+
+
 }
 
-void Recive(Send_args args){
-    
-    
-    string Batched[1000];
+//Recive Recives messages and makes sure taht every message is delivered
+int Recive(Send_args args) {
+
     int NumElem;
     int Failed;
 
-    
-    for (int i = 0; i<1000; i++){
-        if (args.Batched[i] == ""){
+
+    for (int i = 0; i < 1000; i++) {
+        if (args.Batched[i] == "") {
             NumElem = i;
             break;
         }
-        Batched[i] = args.Batched[i];
     }
 
-   
+
     bool LastID = false;
     int ID;
 
     printf("////    Recive Thread Succesfully Started\n");
 
-
+    Send_args Recived;
 
     while (1 == 1) {
         Failed = 0;
-
         ID = ReciveFunc();
-        
+        cout << ID << endl;
 
-        Batched[ID] = "0";
+
+        Recived.Batched[ID] = "0";
 
         if (ID + 1 == NumElem) {
             LastID = true;
         }
 
-        for (int i = 0; i < ID; i++) {
-            if (Batched[i]!="0") {
-                cout << i << endl;
+        for (int i = 0; i < ID + 1; i++) {
+            if (Recived.Batched[i] != "0") {
+
                 Failed++;
-                
-                SendFunc(Batched[i]);
+
+                SendFunc(args.Batched[i]);
             }
         }
+
         if (Failed == 0 and LastID) {
-            cout << "END"<<endl;
-            break;
+
+            cout << "END" << endl;
+
+            return 0;
         }
     }
 
-    printf("//Recive Thread Succesfully ended");
+    return 1;
 }
 
-int Initialize(){
-    
+
+
+int Initialize() {
+
     int ID = -1;
 
 
-    if (WIN){    
+    if (WIN) {
         InitializeWinSock();
     }
 
     InitializeSocket();
     BuildSocket();
+
+
+    return 0;
+
+}
+
+int Send(string msg) {
+
+    cout << "//Starting Winsock Initialize and Socket Build" << endl;
+    Initialize();
+
+
+    Send_args Batched = IntoBatches(msg);
+
+    struct Send_args args;
+
+    for (int i = 0; i < 1000; i++) {
+        if (Batched.Batched[i] == "") {
+            break;
+        }
+        args.Batched[i] = Batched.Batched[i];
+    }
+
+
+
+    std::thread SendingThread(SendWithoutChecking, args);
+    SendingThread.join();
+
+    std::thread ReciveThread(Recive, args);
+    ReciveThread.join();
+
 
 
     return 0;
