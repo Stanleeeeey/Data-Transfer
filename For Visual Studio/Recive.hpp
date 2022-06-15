2514bb0f6d762e0e2d9384a2ef88bb30f5d300f0
mastercode5 <43855236+mastercode5@users.noreply.github.com>
2022-06-15 12:49:45 +0200

    main

--- For Visual Studio/Recive.hpp
+++ For Visual Studio/Recive.hpp
@@ -1,4 +1,4 @@
-#ifdef _WIN32
+/*#ifdef _WIN32
 
 #   include <winsock2.h>
 #   include <Ws2tcpip.h>
@@ -94,7 +94,7 @@ int BuildSocket(string host, int port) {
     LPCWSTR sw = stemp.c_str();
 
     SendSockAddr.sin_family = AF_INET;
-    SendSockAddr.sin_port = htons(port + 1);
+    SendSockAddr.sin_port = htons(port+1);
 
     RecvSockAddr.sin_family = AF_INET;
     RecvSockAddr.sin_port = htons(port);
@@ -127,7 +127,7 @@ string ReciveFunc() {
         return "1";
     }
 
-
+    
     RecvBufferedMsg[recived] = '\0';
 
     string x;
@@ -137,6 +137,7 @@ string ReciveFunc() {
         x.push_back(RecvBufferedMsg[i]);
     }
 
+    
 
     return x;
 
@@ -177,6 +178,7 @@ Message ReciveWithoutInit() {
     int LastID = -2;
 
     while (1 == 1) {
+       
         Recived = 0;
         msg = ReciveFunc();
 
@@ -194,8 +196,8 @@ Message ReciveWithoutInit() {
         SendFunc(msg.substr(0, 3));
 
         FinMessage.value[ID] = msg.substr(3, msg.size());
-
-        if (msg.size() < FinMessage.value[0].size()) {
+        cout << msg.size() << " " << FinMessage.value[0].size() << endl;
+        if (msg.substr(4, msg.size()).size() < FinMessage.value[0].size()) {
 
             cout << "RECIVED LAST MESSAGE" << endl;
             LastID = ID;
@@ -250,8 +252,337 @@ string Recive(string host, int port) {
             ans += X.value[i];
         }
     }
+    cout << "co";
 
-
-
+    cout << ans;
     return ans;
 }
+
+
+
+*/
+#ifdef _WIN32
+
+#   include <winsock2.h>
+#   include <Ws2tcpip.h>
+#   include <windows.h>
+#   pragma comment(lib, "Ws2_32.lib")
+
+
+#   define WIN TRUE
+
+
+#else
+
+#include <sys/sockets.h>
+
+#endif
+
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <typeinfo>
+#include <vector>
+#include <tuple>
+#include <sstream>
+#include <iomanip>
+#include "thread"
+#include <tchar.h>
+#include <chrono>
+#include <atomic>
+
+
+
+using namespace std;
+using namespace std::chrono;
+
+// Zmienne globalne
+#define BUFF 1024
+#define BUFFID 3
+
+char SendMsgBuffered[BUFFID];
+char RecvBufferedMsg[BUFF];
+
+class reciveconnection {
+private:
+    SOCKET SendSock = INVALID_SOCKET;
+    SOCKET RecvSock = INVALID_SOCKET;
+
+    struct sockaddr_in SendSockAddr;
+    struct sockaddr_in RecvSockAddr;
+
+    int port;
+    int timeout;
+    int NumberOfPackets;
+
+    string host;
+
+    struct Message {
+        string value[1000];
+    };
+
+    bool RecivedPackets[1000] = { 0 };
+
+    //SOCKET INITLIALIZATION 
+
+    //initilazaing winsock
+    int InitializeWinSock() {
+
+        WSADATA wsaData;
+        int res = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+        if (res != 0) {
+            printf("//  WSAStartup failed with error %d\n", res);
+            return 1;
+        }
+        printf("//  Winsock Initialized\n");
+        return 0;
+
+    }
+
+
+    //Initializng sockets
+    int check_if_socket_valid() {
+        if (SendSock != INVALID_SOCKET && RecvSock != INVALID_SOCKET) {
+            printf("//  Sockets succesfully initialized\n");
+            return 0;
+
+        }
+
+        printf("//  Sockets initialization failed %d\n", WSAGetLastError());
+        return 1;
+    }
+    int InitializeSocket() {
+
+
+        SendSock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+        RecvSock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+        return check_if_socket_valid();
+
+    }
+
+    void adress_sockets() {
+        SendSockAddr.sin_family = AF_INET;
+        SendSockAddr.sin_port = htons(port+1);
+
+        RecvSockAddr.sin_family = AF_INET;
+        RecvSockAddr.sin_port = htons(port);
+
+    }
+
+    void convert_sockets_to_bin() {
+
+        wstring stemp = std::wstring(host.begin(), host.end());
+        LPCWSTR sw = stemp.c_str();
+
+        InetPton(AF_INET, sw, &RecvSockAddr.sin_addr.s_addr);
+        InetPton(AF_INET, sw, &SendSockAddr.sin_addr.s_addr);
+    }
+
+    int attempt_biniding_recive_sock() {
+        if (bind(RecvSock, (SOCKADDR*)&RecvSockAddr, sizeof(RecvSockAddr))) {
+            printf("//  bind failed with error %d\n", WSAGetLastError());
+            return 1;
+        }
+
+        printf("//  Socket succesfully binded\n");
+        return 0;
+    }
+    //build sockets
+    int BuildSocket() {
+
+        adress_sockets();
+        convert_sockets_to_bin();
+        return attempt_biniding_recive_sock();
+
+    }
+
+    int CloseSocket() {
+
+        closesocket(SendSock);
+        closesocket(RecvSock);
+        WSACleanup();
+        return 1;
+    }
+
+    int Initialize() {
+
+        if (WIN) {
+            InitializeWinSock();
+        }
+
+
+        if (InitializeSocket() || BuildSocket()) {
+            printf("aborting process due to above error");
+            return 1;
+        }
+
+
+        return 0;
+    }
+
+
+    // Functions to recive data from client 
+
+    void is_message_valid(int recived) {
+        if (recived == SOCKET_ERROR) {
+            printf("// recvfrom failed with error %d\n", WSAGetLastError());
+            abort();
+        }
+    }
+
+    string single_packet_recive() {
+        char RecvBufferedMsg[BUFF];
+
+        int ServerAddrSize = sizeof(RecvSockAddr);
+
+
+        int recived = recvfrom(RecvSock, RecvBufferedMsg, 1024, 0, (SOCKADDR*)&RecvSockAddr, &ServerAddrSize);
+
+        if (recived == SOCKET_ERROR) {
+            printf("// recvfrom failed with error %d\n", WSAGetLastError());
+            return "1";
+        }
+
+
+        RecvBufferedMsg[recived] = '\0';
+
+        string x;
+
+        for (int i = 0; i < recived; i++) {
+
+            x.push_back(RecvBufferedMsg[i]);
+        }
+
+
+        
+        return x;
+    }
+
+    
+
+
+    //Functions to Send
+
+    bool is_socket_error(int result) {
+        if (result == SOCKET_ERROR) {
+            printf("// while sending got an error: %d\n", WSAGetLastError());
+            return 1;
+        }
+
+        return 0;
+    }
+
+    int send_single_packet(std::string x) {
+
+
+
+        for (int i = 0; i < x.length(); i++) {
+            SendMsgBuffered[i] = x[i];
+        }
+
+        int result = sendto(SendSock, SendMsgBuffered, x.length(), 0, (SOCKADDR*)&SendSockAddr, sizeof(SendSockAddr));
+
+        return is_socket_error(result);
+    }
+
+
+
+    string recive_and_resend() {
+        /// 
+        /// recives data resend if needed
+        /// 
+        
+        
+        int Recived = 0;
+        string msg;
+
+        int FinMessage[10000] = {0};
+
+
+        int ID;
+        int LastID = -2;
+        int size = 0;
+
+        string ans = "";
+
+        while (1 == 1) {
+            Recived = 0;
+            msg = single_packet_recive();
+
+
+            if (msg.substr(1, 3) == "000") {
+                ID = 0;
+                ans += msg.substr(3, msg.size());
+            }
+            else {
+                std::stringstream str(msg.substr(0, 3));
+                str >> ID;
+                ans += msg.substr(3, msg.size());
+
+            }
+
+
+            send_single_packet(to_string(ID));
+            
+            FinMessage[ID] = msg.size();
+            if (FinMessage[0] > msg.size()) {
+                LastID = ID;
+                cout << "LAST ID RECIVED\n";
+            }
+
+            if (LastID != -1) {
+                for (int i = 0; i <= LastID; i++) {
+
+                    if (FinMessage[i])
+                        Recived++;
+                }
+            }
+            
+            if (Recived == LastID + 1) {
+                cout << "END" << endl;
+                break;
+            }
+
+
+        }
+
+        return ans;
+
+    }
+
+
+
+public:
+    reciveconnection(string userhost, int userport, int usertimeout = 1) {
+        host = userhost;
+        port = userport;
+        timeout = usertimeout;
+        Initialize();
+
+    }
+
+
+
+    string Recive() {
+        
+        string ans = recive_and_resend();
+
+        CloseSocket();
+
+        return ans;
+    }
+
+};
+
+
+
+
+
+
+
+string Recive(string host, int port) {
+    reciveconnection x = reciveconnection(host, port);
+    return x.Recive();
+    }
